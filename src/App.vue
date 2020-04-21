2147aa39a41ad660f31125db5d496fcf31de6203
tesao <shimeji9@gmail.com>
2020-04-21 21:44:46 +0900

    reactive, computed の実装

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Double />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Double from '@/components/Double.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Double
   }
 }
 </script>
---
+++ src/components/Double.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div>
+    <input type="number" v-model="state.input_value" /><br>
+    Input value is {{state.input_value}} and its double is {{state.double}}
+  </div>
+</template>
+
+<script>
+import { reactive, computed } from '@vue/composition-api'
+
+export default {
+  setup() {
+    const state = reactive({
+      input_value: 0,
+      double: computed(() => state.input_value * 2)
+    })
+
+    return {
+      state
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
